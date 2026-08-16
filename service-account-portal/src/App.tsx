7e47a5175c4029b8c7858189a0be9ac4d4ea09ee
seanr89 <srafferty89@gmail.com>
2026-08-16 20:15:01 +0100

    refactor: remove onRequestClick prop from Navbar and update related components

--- service-account-portal/src/App.tsx
+++ service-account-portal/src/App.tsx
@@ -86,7 +86,6 @@ export default function App() {
         <Navbar
           filesCount={localFilesCount}
           accountsCount={accounts.length}
-          onRequestClick={() => handleOpenRequestModal()}
         />
 
         <main className="flex-1 max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-8">
